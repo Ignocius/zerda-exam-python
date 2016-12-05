96f7eb8beb6289b2849e46a9dd88b48364c34406
Szabo Rihcard <ignocius@gmail.com>
2016-12-05 09:47:17 +0100

    Third task complete, but I should check, not so sure ~

---
+++ testthird.py
@@ -0,0 +1,20 @@
+import unittest
+from third import count_letter_in_string
+
+class UnittestForThirdTask(unittest.TestCase):
+    def test_if_normal_string_input_works(self):
+        self.assertEqual(count_letter_in_string("apple","a"), 1)
+
+    def test_if_upparcase_string_input_works(self):
+        self.assertEqual(count_letter_in_string("Apple","a"), 1)
+
+    def test_if_empty_input_works(self):
+        self.assertEqual(count_letter_in_string("",""), 0)
+
+    def test_if_we_use_number_input(self):
+        self.assertEqual(count_letter_in_string(123, 2), 0)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
