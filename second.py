0603d6f6805a688e63c998555648d0f065f8d8e1
Szabo Rihcard <ignocius@gmail.com>
2016-12-05 09:30:23 +0100

    Second task complete, +tree.txt added

--- second.py
+++ second.py
@@ -4,3 +4,15 @@
 # If any problem raises with the file output, the function should not break, but return False.
 # Example: when called with the following two parameters: "tree.txt", "apple",
 # the function should write "appleappleapple" to the file "tree.txt", and return True.
+
+def file_writer_ten_times_in_one_run(file_name, string_input):
+    try:
+        f = open(file_name, "w")
+        for file_writer_runner in range(10):
+            f.write(string_input)
+        return True
+    except IOError:
+        print(False)
+
+file_writer_ten_times_in_one_run("tree.txt", "apple")
+file_writer_ten_times_in_one_run("", "apple")
